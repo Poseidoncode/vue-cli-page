08b28026b56108b64db66363b82652f9764b8286
Poseidoncode <66759971+Poseidoncode@users.noreply.github.com>
2021-03-25 22:23:25 +0800

    qq

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <h1>test</h1>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
